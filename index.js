46be8c3f39883a60b0db96c49a8d902183bbae9f
Kenwanjohi <wanjohiken85@gmail.com>
2020-10-09 18:40:11 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,117 @@
+const express = require('express')
+const exphbs = require("express-handlebars")
+const buildUrl = require('build-url');
+const axios = require('axios');
+const session = require('express-session');
+const crypto = require('crypto');
+const querystring = require('querystring');
+require('dotenv').config()
+const app = express()
+app.set('view engine', 'hbs');
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+    }));
+const port =  process.env.PORT || 3000
+const client = {
+    client_id: process.env.Client_id,
+    client_secret: process.env.Client_secret,
+    redirect_uri: "https://fathomless-escarpment-03995.herokuapp.com/callback"
+}
+const authProvider = {
+    authEndpoint: 'https://accounts.spotify.com/authorize',
+    tokenEndpoint:  'https://accounts.spotify.com/api/token'
+}
+app.get('/', (req, res) => {
+    res.render('home')
+});
+let state 
+crypto.randomBytes(20, (err, buf) => {
+  if (err) throw err;
+  state = buf.toString('hex')
+});
+let access_token = null
+app.use(session({
+    name: 'SESSION_ID',
+    secret: process.env.secret,
+    resave: true,
+    saveUninitialized: true,
+  }))
+
+app.get('/authorize', (req, res) => {
+    access_token = null
+
+const authorizeUrl = buildUrl(authProvider.authEndpoint, {
+    queryParams: {
+        client_id: client.client_id,
+        redirect_uri: client.redirect_uri,
+        state,
+        response_type: "code",
+        scope: "user-top-read"
+    }
+});
+    req.session.state = state
+    res.redirect(authorizeUrl);
+})
+const stringToBase64 = (clientId, clientSecret) => {
+    const str = `${clientId}:${clientSecret}`
+    const buff = Buffer.from(str, 'utf8');
+    return buff.toString('base64');
+}
+app.get('/callback', (req, res) =>{
+    if(req.query.state !== req.session.state) {
+        res.render('error', {error: 'State doesn\'t match'})
+    }
+    const code = req.query.code;
+    const accessToken = async () => {
+        try {
+            const response = await axios({
+                method: 'post',
+                url: authProvider.tokenEndpoint,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Basic ' + stringToBase64(client.client_id, client.client_secret)
+                },
+                data: querystring.stringify({
+                    code,
+                    grant_type: "authorization_code",
+                    redirect_uri: client.redirect_uri
+                })
+            })
+            console.log(response.data)
+            access_token = response.data.access_token
+            res.redirect('/welcome')
+        } catch (err) {
+            console.log(err)
+            return
+        }
+    }
+    accessToken()   
+})
+app.get('/welcome', (req, res) => {
+if(access_token) {
+    const fetch_lists = async () => {
+        var headers = {
+            'Authorization': 'Bearer ' + access_token
+            };
+        try{
+            const response = await axios({
+                method: 'get',
+                headers: headers,
+                url: 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=50'
+
+            })
+            res.render('welcome', {items: response.data.items})
+        } catch (error) {
+            console.log(err)
+            return
+        }   
+    }
+    fetch_lists()
+} else {
+    console.log('error')
+}
+})
+app.listen(port, () => {
+    // console.log(`Spotylistslistening at http://localhost:${port}`)
+  })
